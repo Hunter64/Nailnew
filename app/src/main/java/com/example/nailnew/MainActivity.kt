122a3fad44cacc6ed1bfedb8d47076f0f646d5e6
Héctor López <hector.lopez@DSA-DEV2-10>
2019-08-01 15:39:20 -0500

    add snackbar message example

--- app/src/main/java/com/example/nailnew/MainActivity.kt
+++ app/src/main/java/com/example/nailnew/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.nailnew
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,6 +13,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Toast.makeText(this, "Hello from the Toast!", Toast.LENGTH_LONG).show()
+        //Toast
+        //Toast.makeText(this, "Hello from the Toast!", Toast.LENGTH_LONG).show()
+
+        //---------------------------------------------------------------------
+
+        //SnackBar (2 examples)
+        val layout = findViewById<ConstraintLayout>(R.id.constraint)
+
+        //Snackbar.make(layout, "Hello from the SnackBar!", Snackbar.LENGTH_LONG).show()
+
+        Snackbar.make(layout, "Hello from the SnackBar!", Snackbar.LENGTH_LONG).setAction("Undo Changes!"){
+            Log.w(" -> SnackBar", "Add Function / Action here! for Click Undo Changes!")
+            Snackbar.make(layout, "Action has been restored!", Snackbar.LENGTH_LONG).show()
+        }.show()
     }
 }
