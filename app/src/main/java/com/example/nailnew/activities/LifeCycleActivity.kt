61dfd878aff7f024377e02ff62a04a760f4e8e37
Héctor López <hector.lopez@DSA-DEV2-10>
2019-08-02 12:56:01 -0500

    use and customize function onBackPressed() with Toast message and Handler postDelay

--- app/src/main/java/com/example/nailnew/activities/LifeCycleActivity.kt
+++ app/src/main/java/com/example/nailnew/activities/LifeCycleActivity.kt
@@ -1,13 +1,29 @@
 package com.example.nailnew.activities
 
 import android.os.Bundle
+import android.os.Handler
+import android.widget.Toast
 import com.example.nailnew.R
 
 class LifeCycleActivity : LifeCycleEventsActivity() {
 
+    private var exitEnabled = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_life_cycle)
+    }
+
+    override fun onBackPressed() {
+        //This is the button back arrow... Here call to father
+        //super.onBackPressed() //If we delete this line, the back button is disabled
+
+        if(exitEnabled)
+            super.onBackPressed()
+
+        exitEnabled = true
+        Toast.makeText(this, "Click again to exit this screen!", Toast.LENGTH_LONG).show()
+        Handler().postDelayed(Runnable { exitEnabled = false }, 2000)
     }
 
 }
