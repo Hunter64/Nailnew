b09d696036abc226d2691d0f457937d3228cfbff
Héctor López <hector.lopez@DSA-DEV2-10>
2019-08-03 08:33:31 -0500

    using the kotlin android extensions, with this we don't use the find by id any more, because we have access all elements in xml file declared with Id

--- app/src/main/java/com/example/nailnew/activities/KotlinAndroidExtensionActivity.kt
+++ app/src/main/java/com/example/nailnew/activities/KotlinAndroidExtensionActivity.kt
@@ -2,12 +2,27 @@ package com.example.nailnew.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import com.example.nailnew.R
+import kotlinx.android.synthetic.main.activity_kotlin_android_extension.* //Load all have in layout "activity kotlin android extension",
+// when import this, we no need use findById any more, because we call the buttons directly with his name, that is same name by Id, without need cast this is very cool :D... I like this!!
 
 class KotlinAndroidExtensionActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kotlin_android_extension)
+
+
+        //This form is using find view by id
+        val btn = findViewById<Button>(R.id.buttonById)
+        btn.setOnClickListener { Toast.makeText(this, "Click by Id", Toast.LENGTH_SHORT).show() }
+
+
+        //This form we can access to buttons, layouts, and any element that stay with a id in activity kotlin android extension file xml, thanks to the "kotlin android extensions", putting the same name to Id,
+        //with this form don't need the "find by Id" any more
+        buttonById.setOnClickListener { Toast.makeText(this, "Click by Id!", Toast.LENGTH_SHORT).show() }  //-> ButtonById from activity_kotlin_android_extension.xml
+        buttonByKAT.setOnClickListener { Toast.makeText(this, "Click by KAT", Toast.LENGTH_SHORT).show() } //-> ButtonBy KAT from activity_kotlin_android_extension.xml
     }
 }
