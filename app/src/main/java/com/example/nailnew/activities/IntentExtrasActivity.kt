9a0b811accb22bf2b77b4712e0ac831fba863438
Hunter <lopezhector233@gmail.com>
2019-08-04 21:35:53 -0500

    create intent extras activity to receive values from intents activity and show this in text view and checkbox

---
+++ app/src/main/java/com/example/nailnew/activities/IntentExtrasActivity.kt
@@ -0,0 +1,14 @@
+package com.example.nailnew.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.nailnew.R
+
+class IntentExtrasActivity : AppCompatActivity() {
+//In this activity, we receive values from intents activity and show in the text view and checkbox
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intent_extras)
+    }
+}
--- app/src/main/java/com/example/nailnew/activities/IntentsActivity.kt
+++ app/src/main/java/com/example/nailnew/activities/IntentsActivity.kt
@@ -1,13 +1,37 @@
 package com.example.nailnew.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.nailnew.R
+import kotlinx.android.synthetic.main.activity_intents.*
 
 class IntentsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intents)
+
+        buttonIntentExtras.setOnClickListener { goIntentExtras() }
+        buttonIntentFlags.setOnClickListener { goIntentFlags() }
+        buttonIntentObject.setOnClickListener { goIntentObject() }
+    }
+
+    //Extras
+    private fun goIntentExtras(){
+        val intent = Intent(this, IntentExtrasActivity::class.java)
+        startActivity(intent)
+    }
+
+    //Flags
+    private fun goIntentFlags(){
+        val intent = Intent(this, IntentExtrasActivity::class.java)
+        startActivity(intent)
+    }
+
+    //Objects
+    private fun goIntentObject(){
+        val intent = Intent(this, IntentExtrasActivity::class.java)
+        startActivity(intent)
     }
 }
