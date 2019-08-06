1dc1fd436327ada6a657c81004d1a1ef59efefe0
Héctor López <hector.lopez@dsa-dev2-10>
2019-08-06 17:59:57 -0500

    working to adding extension functions

--- app/src/main/java/com/example/nailnew/activities/ExtensionFunctionsActivity.kt
+++ app/src/main/java/com/example/nailnew/activities/ExtensionFunctionsActivity.kt
@@ -3,11 +3,28 @@ package com.example.nailnew.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.nailnew.R
+import com.example.nailnew.others.isNatural
+import com.example.nailnew.others.loadByUrl
+import com.example.nailnew.others.snackBar
+import com.example.nailnew.others.toast
+import kotlinx.android.synthetic.main.activity_extension_functions.*
 
 class ExtensionFunctionsActivity : AppCompatActivity() {
+    //The extensions function is very powerful, only check this next exercises
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_extension_functions)
+
+        var numberRandom = (-10..10).random()
+
+        buttonToast.setOnClickListener { if(numberRandom.isNatural()) toast("$numberRandom is Natural Number!!") else toast("$numberRandom isn't Natural Number!!") } //-> //Test boolean is natural extension function with extension toast function
+                                                // -> Test toast extension function, here pass a simple string (quote marks) also pass string from string file's
+                                                //toast(getString(R.string.extensions_toast)) || buttonToast.setOnClickListener { toast(R.string.extensions_toast) }  //, this because in library receive string or id to string of resource
+
+
+        buttonSnackBar.setOnClickListener { snackBar("I love extension function!", action = "Undo") { toast("Undo!!!") } }
+        buttonLoadByUrl.setOnClickListener { imageViewLoadedByUrl.loadByUrl("https://udemy-images.udemy.com/course/480x270/1325930_f5f6_3.jpg") }
+        buttonGoToActivity.setOnClickListener {  }
     }
 }
---
+++ app/src/main/java/com/example/nailnew/others/ExtensionFunctions.kt
@@ -0,0 +1,45 @@
+package com.example.nailnew.others
+
+import android.app.Activity
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+import com.example.nailnew.R
+import com.squareup.picasso.Picasso
+
+fun Int.isNatural() = this >= 0 //-> Natural numbers, condition return true or false, "this" is reference of context
+
+
+fun Activity.toast(message: CharSequence, duration: Int = Toast.LENGTH_SHORT) = Toast.makeText(this, message, duration).show() // -> Toast that receive msg and duration, if duration isn't take duration by default that is length short,
+                                                                                                    // "this" is reference into context of activity
+
+fun Activity.toast(resourceId: Int, duration: Int = Toast.LENGTH_SHORT) = Toast.makeText(this, resourceId, duration).show() //-> This is for use string of string file's in values folder's, that is int for id of string
+
+
+fun Activity.snackBar(message: CharSequence, view: View? = findViewById(R.id.container), duration: Int = Snackbar.LENGTH_SHORT, action: String? = null, actionEvt: (v: View) -> Unit = {}){
+    if(view != null){
+        val snackBar = Snackbar.make(view, message, duration) //if check with view != null isn't, we should put view with !! to didn't run this -> view!!
+        if(!action.isNullOrEmpty()){
+            snackBar.setAction(action, actionEvt)
+        }
+        snackBar.show()
+    }
+}
+
+
+fun ViewGroup.inflate(layoutId: Int) = LayoutInflater.from(context).inflate(layoutId,this,false)!!
+
+
+fun ImageView.loadByUrl(url: String) = Picasso.with(context).load(url).into(this)
+
+
+inline fun <reified T:Activity> Activity.goToActivity(init: Intent.() -> Unit){ //Callback of intent restricted
+    val intent = Intent(this, T::class.java)
+    intent.init()
+    startActivity(intent)
+}
+
